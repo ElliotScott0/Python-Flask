5712bea26ca7de9be754daf92cf971ec2aee152a
ElliotScott0 <elliotscott02@gmail.com>
2023-12-29 21:09:20 +0000

    reverted changes

--- catalogue.py
+++ catalogue.py
@@ -75,7 +75,7 @@ def cat_page():
               html=html+'<h3>'+name+'</h3>'
               ServerIP=request.host.split(':')[0]
               html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-              html=html+'<img src="http://34.89.3.47/pics/'+thumb+'">'
+              html=html+'<img src="http://35.246.124.242/pics/'+thumb+'">'
               html=html+"</a>"        
               print("=======================")
 
